2e1e700913d41730ceaf8b6468ca1b205b43ccf9
SpiceMuran94 <headiv94@gmail.com>
2022-03-06 22:52:23 +0900

    修正

--- color_palette_generator/src/components/ColorList.vue
+++ color_palette_generator/src/components/ColorList.vue
@@ -4,15 +4,13 @@
 
 
     <ColorItem name="Blue"   hue="216" :saturation="colorData.saturation" :lightness="colorData.lightness"/>
-    <ColorItem name="Indigo" hue="252" :saturation="colorData.saturation" :lightness="colorData.lightness"/>
-    <ColorItem name="Purple" hue="288" :saturation="colorData.saturation" :lightness="colorData.lightness"/>
-    <ColorItem name="Pink"   hue="324" :saturation="colorData.saturation" :lightness="colorData.lightness"/>
-    <ColorItem name="Red"    hue="0"   :saturation="colorData.saturation" :lightness="colorData.lightness"/>
-    <ColorItem name="Orange" hue="36"  :saturation="colorData.saturation" :lightness="colorData.lightness"/>
-    <ColorItem name="Yellow" hue="72"  :saturation="colorData.saturation" :lightness="colorData.lightness"/>
-    <ColorItem name="Green"  hue="108" :saturation="colorData.saturation" :lightness="colorData.lightness"/>
-    <ColorItem name="Teal"   hue="144" :saturation="colorData.saturation" :lightness="colorData.lightness"/>
-    <ColorItem name="Cyan"   hue="180" :saturation="colorData.saturation" :lightness="colorData.lightness"/>
+    <ColorItem name="Purple" hue="261" :saturation="colorData.saturation" :lightness="colorData.lightness"/>
+    <ColorItem name="Pink"   hue="330" :saturation="colorData.saturation" :lightness="colorData.lightness"/>
+    <ColorItem name="Red"    hue="354"   :saturation="colorData.saturation" :lightness="colorData.lightness"/>
+    <ColorItem name="Orange" hue="27"  :saturation="colorData.saturation" :lightness="colorData.lightness"/>
+    <ColorItem name="Yellow" hue="45"  :saturation="colorData.saturation" :lightness="colorData.lightness"/>
+    <ColorItem name="Green"  hue="152" :saturation="colorData.saturation" :lightness="colorData.lightness"/>
+    <ColorItem name="Cyan"   hue="190" :saturation="colorData.saturation" :lightness="colorData.lightness"/>
   </div>
 </template>
 
@@ -28,8 +26,8 @@
     data() {
       return {
         colorData: {
-          saturation: "70",
-          lightness: "70",
+          saturation: "80",
+          lightness: "60",
         }
       }
     },
